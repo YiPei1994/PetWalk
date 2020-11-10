fc1aa4e28229ffd0f8b549bb3c5110e9900bc8e3
YiPei1994 <studentypz@gnail.com>
2020-11-10 15:54:54 +0100

    update

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 (function($) {          
     $(document).ready(function(){ 
+        
+        wow = new WOW(
+            {
+            boxClass:     'wow',      // default
+            animateClass: 'animated', // default
+            offset:       0,          // default
+            mobile:       true,       // default
+            live:         true        // default
+          }
+          )
+          wow.init();
+
         $('nav').hide();
 
         $(window).scroll(function(){                          
